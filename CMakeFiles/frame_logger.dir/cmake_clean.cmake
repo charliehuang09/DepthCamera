f83aff3506fab5f523ed4c6d3faae35338d3ad76
Charlie Huang <charliehuang09@gmail.com>
2025-08-16 13:30:11 -0700

    Initial commit of gamepiece detection (pls commit more in future Charlie)

---
+++ CMakeFiles/frame_logger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frame_logger.dir/frame_logger.cc.o"
+  "CMakeFiles/frame_logger.dir/frame_logger.cc.o.d"
+  "frame_logger"
+  "frame_logger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frame_logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
